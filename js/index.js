0181abfa084cc3db0d83e7be9f157e1965bcae16
nandita-codeclouds <nandita.sasha@codeclouds.co.in>
2022-11-25 19:01:44 +0530

    added responsiveness

--- js/index.js
+++ js/index.js
@@ -6,15 +6,12 @@ function openTabImg(img){
     expandImg.parentElement.style.display = "block";
 }
 
-
 function checkOfferValue(element){
     console.log(element.value);
     offerValue = element.value;
     var subscribeOption = document.getElementById('subscribe_and_save_options');
     var oneTimeOption = document.getElementById('one_time_options');
-   
-//    subscribeOption.classList.add('show');
-//    oneTimeOption.classList.add('hide');
+
    if(offerValue == 'subscribe_and_save'){
     // console.log(subscribeOption);
     oneTimeOption.classList.remove('show');
@@ -43,4 +40,31 @@ function checkOfferValue(element){
     
    }
 
+}
+
+function selectInput(element){
+
+}
+
+var inputs = document.getElementsByClassName("offer-input-button");
+// console.log(inputs);
+for (var i = 0; i < inputs.length; i++) {
+inputs[i].addEventListener("click", function() {
+var current = document.getElementsByClassName("active");
+// console.log(current);
+current[0].className = current[0].className.replace(" active", "");
+this.className += " active";
+});
+}
+
+
+var inputs = document.getElementsByClassName("offer-button");
+// console.log(inputs);
+for (var i = 0; i < inputs.length; i++) {
+inputs[i].addEventListener("click", function() {
+var current = document.getElementsByClassName("active");
+// console.log(current);
+current[0].className = current[0].className.replace(" active", "");
+this.className += " active";
+});
 }
